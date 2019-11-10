7ed9ab26b58b35946b669d4f91acb83ca621a75a
jaiveerbhaidasna <43055551+jaiveerbhaidasna@users.noreply.github.com>
2019-11-09 23:18:07 -0600

    All Classes Written
    Onto driver gameplay!

--- player.cpp
+++ player.cpp
@@ -2,3 +2,69 @@
 // Created by Jaiveer Bhaidasna on 11/8/2019.
 //
 
+#include "player.h"
+#include "deck.h"
+
+using namespace std;
+
+    string Player::showHand() const {
+        string hand;
+        for(int i = 0; i < myHand.size(); i++)
+            hand += myHand[i].toString() +  " ";
+        return hand;
+    }
+
+    string Player::showBooks() const {
+        string books;
+        for(int i = 0; i < myBook.size(); i+=2)
+            books += myBook[i].toString() + " & " + myBook[i+1].toString() + "\n";
+        return books;
+    }
+
+    void Player::addCard(Card c) {
+        myHand.push_back(c);
+    }
+
+    void Player::bookCards(Card c1, Card c2) {
+        myBook.push_back(c1);
+        myBook.push_back(c2);
+    }
+
+    bool Player::rankInHand(Card c) const {
+        for(int i = 0; i < myHand.size(); i++)
+            if(c.getRank() == myHand[i].getRank())
+                return true;
+            return false;
+    }
+
+    bool Player::cardInHand(Card c) const {
+        for(int i = 0; i < myHand.size(); i++)
+            if(c == myHand[i])
+                return true;
+            return false;
+    }
+
+    Card Player::removeCardFromHand(Card c) {
+        for(int i = 0; i < myHand.size(); i++)
+            if (c == myHand[i])
+                myHand.erase(myHand.begin() + i);
+        return c;
+    }
+
+    bool Player::checkHandForBook(Card &c1, Card &c2) {
+        for(int i = 0; i < myHand.size()-1; i++)
+            for(int j = i + 1; j < myHand.size(); j++)
+            {
+                if(myHand[i].getRank() == myHand[j].getRank())
+                {
+                    c1 = myHand[i];
+                    c2 = myHand[j];
+                    return true;
+                }
+            }
+        return false;
+    }
+
+    Card Player::chooseCardFromHand() const {
+        return myHand[rand() % myHand.size()];
+    }
--- player.h
+++ player.h
@@ -10,11 +10,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-
 #include "card.h"
 using namespace std;
-
 
 class Player
 {
@@ -57,22 +54,24 @@ class Player
     string showHand() const; 
     string showBooks() const; 
     
-    int getHandSize() const; 
-    int getBookSize() const; 
+    int getHandSize() const{
+        return myHand.size();
+    }
+    int getBookSize() const{
+        return myBook.size()/2;
+    };
        
     //OPTIONAL
     // comment out if you decide to not use it
     //this function will check a players hand for a pair. 
     //If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
-
-    bool checkHandForPair(Card &c1, Card &c2);
+    //bool checkHandForPair(Card &c1, Card &c2);
 
     //OPTIONAL
     // comment out if you decide to not use it    
     //Does the player have a card with the same rank as c in her hand?
     //e.g. will return true if the player has a 7d and the parameter is 7c
-    
-    bool sameRankInHand(Card c) const; 
+    //bool sameRankInHand(Card c) const;
     
     
   private:
